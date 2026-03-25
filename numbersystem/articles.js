15f6cf916c02a937521776fc63e916d3569bbe5d
lllllllllwith10ls <zhangluke0528@gmail.com>
2026-03-24 19:06:19 -0500

    First update in almost a year

---
+++ numbersystem/articles.js
@@ -0,0 +1,14 @@
+const urlString = window.location.href;
+const url = new URL(urlString);
+
+function loadArticle(id) {
+    document.getElementById("leftcolumn").innerHTML = document.getElementById("article" + id).innerHTML;
+}
+
+maxArticle = 0;
+
+if (url.searchParams.get("article") == null || isNaN(parseInt(url.searchParams.get("article"))) || parseInt(url.searchParams.get("article")) > maxArticle) {
+    loadArticle(maxArticle);
+} else {
+    loadArticle(parseInt(url.searchParams.get("article")));
+}
