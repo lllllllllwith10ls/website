ef40267f60f3971f56dcfa78e6f7a5a8504cf38a
lllllllllwith10ls <zhangluke0528@gmail.com>
2026-04-01 11:56:12 -0500

    Number system of the week.

--- numbersystem/articles.js
+++ numbersystem/articles.js
@@ -5,7 +5,7 @@ function loadArticle(id) {
     document.getElementById("leftcolumn").innerHTML = document.getElementById("article" + id).innerHTML;
 }
 
-maxArticle = 0;
+maxArticle = 1;
 
 if (url.searchParams.get("article") == null || isNaN(parseInt(url.searchParams.get("article"))) || parseInt(url.searchParams.get("article")) > maxArticle) {
     loadArticle(maxArticle);
