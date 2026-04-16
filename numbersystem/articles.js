410a480cfb18019167d2e40d62c3704659b102c6
lllllllllwith10ls <zhangluke0528@gmail.com>
2026-04-16 14:23:06 -0500

    number system of the week.

--- numbersystem/articles.js
+++ numbersystem/articles.js
@@ -5,7 +5,7 @@ function loadArticle(id) {
     document.getElementById("leftcolumn").innerHTML = document.getElementById("article" + id).innerHTML;
 }
 
-maxArticle = 2;
+maxArticle = 3;
 
 if (url.searchParams.get("article") == null || isNaN(parseInt(url.searchParams.get("article"))) || parseInt(url.searchParams.get("article")) > maxArticle) {
     loadArticle(maxArticle);
